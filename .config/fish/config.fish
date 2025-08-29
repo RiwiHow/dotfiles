c6947a8038ab6908accace04c191c72738464079
RiwiHow <53964574+RiwiHow@users.noreply.github.com>
2025-08-29 16:19:00 +0800

    add global variable for bun

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,17 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# macOS
+set -gx XDG_CONFIG_HOME $HOME/.config
+
 # homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # bun
 set -gx BUN_INSTALL_CACHE_DIR $HOME/.cache/bun
+set -gx BUN_INSTALL_GLOBAL_DIR $HOME/.local/bun/global
+set -gx BUN_INSTALL_BIN $HOME/.local/bun/bin
 
 # less
 set -gx LESSHISTFILE -
 
 # uv
-set -gx PATH /Users/riwihow/.local/bin $PATH
+set -gx PATH /Users/riwihow/.local/uv/bin $PATH
 
 # gnupg
 set -gx GNUPGHOME /Users/riwihow/.config/gnupg
